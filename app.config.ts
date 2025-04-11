9e2bbc2330ca3a597c5beb4b0b53d63cb0d9d280
doriantokarz <115377961+doriantokarz@users.noreply.github.com>
2025-04-11 16:43:55 +0200

    personnalisation app settings

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,7 @@
 export default defineAppConfig({
-  title: 'Sink',
-  email: 'sink.cool@miantiao.me',
-  github: 'https://github.com/ccbikai/sink',
-  twitter: 'https://sink.cool/kai',
-  telegram: 'https://sink.cool/telegram',
-  mastodon: 'https://sink.cool/mastodon',
-  blog: 'https://sink.cool/blog',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
+  title: 'Caliko Link Shortener',
+  email: 'contact@caliko.net',
+  description: 'Caliko link shortener',
   image: 'https://sink.cool/banner.png',
   previewTTL: 24 * 3600, // 24h
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
